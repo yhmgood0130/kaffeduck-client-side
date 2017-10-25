6702b761894f2c27c3eaef4b79259f9e3cc61ac8
Hyunmo <yhmgood0130@gmail.com>
2017-10-24 22:30:42 -0600

    action/reducers added

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
+import { Provider } from 'react-redux';
+import configureStore from './configureStore';
 
-AppRegistry.registerComponent('kaffeduck', () => App);
+const store = configureStore();
+
+const kaffeduck = () => {
+  <Provider store={store}>
+    <App />
+  </Provider>
+}
+
+AppRegistry.registerComponent('kaffeduck', () => kaffeduck);
