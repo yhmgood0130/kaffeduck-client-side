5f1c7b2738f93f823329a331d83cf3eb0fbeeb77
Hyunmo <yhmgood0130@gmail.com>
2017-10-28 13:52:58 -0600

    Contact info added

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Dimensions, Image, ImageBackground } from 'react-native';
+import { View, Text, StyleSheet, Dimensions, Image, ImageBackground, TouchableOpacity, Linking } from 'react-native';
 import { connect } from 'react-redux';
 import { Actions } from 'react-native-router-flux';
 
@@ -13,13 +13,28 @@ const Contact = () => {
             style={styles.picture}
             />
         </View>
-        <Text style={styles.social}>Hyunmo "Moe" Yang</Text>
+        <Text style={styles.name}>Hyunmo (Moe)</Text>
         <View style={styles.socialContainer}>
-          <Text style={styles.social}>Email</Text>
-          <Text style={styles.social}>Phone</Text>
-          <Text style={styles.social}>GitHub</Text>
-          <Text style={styles.social}>Facebook<Image source={require('../../images/facebook.png')} style={styles.socialLogo} /></Text>
-          <Text style={styles.social}>LinkedIn</Text>
+          <TouchableOpacity onPress={Actions.home} style={styles.socialStyle} activeOpacity={0.5}>
+            <Image source={require('../../images/phone.png')} style={styles.socialLogo} />
+            <Text style={styles.social}>(720)-545-7597</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={Actions.home} style={styles.socialStyle} activeOpacity={0.5}>
+            <Image source={require('../../images/email.png')} style={styles.socialLogo} />
+            <Text style={styles.email}>yhmgood0130@gmail.com</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => Linking.openURL('https://github.com/yhmgood0130/')} style={styles.socialStyle} activeOpacity={0.5}>
+            <Image source={require('../../images/github.png')} style={styles.socialLogo} />
+            <Text style={styles.social}>/yhmgood0130</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => Linking.openURL('https://www.facebook.com/hyunmoyang13')} style={styles.socialStyle} activeOpacity={0.5}>
+            <Image source={require('../../images/facebook.png')} style={styles.socialLogo} />
+            <Text style={styles.social}>/hyunmoyang13</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => Linking.openURL('https://www.linkedin.com/in/hyunmoyang/')} style={styles.socialStyle} activeOpacity={0.5}>
+            <Image source={require('../../images/linkedin.png')} style={styles.socialLogo} />
+            <Text style={styles.social}>/in/hyunmoyang/</Text>
+          </TouchableOpacity>
         </View>
       </ImageBackground>
     </View >
@@ -42,31 +57,62 @@ const styles = StyleSheet.create({
   },
   picture: {
     width: Dimensions.get('window').width,
-    height:200,
-    width:200,
-    borderRadius:100,
+    height:150,
+    width:150,
+    borderRadius:75,
   },
   backgroundImage: {
     flex: 1,
     width: null,
     height: null,
   },
+  name: {
+    padding:10,
+    fontSize: 34,
+    fontWeight: 'bold',
+    color: "#FFFFFF",
+    backgroundColor: 'rgba(0,0,0,0)',
+    textAlign: 'center',
+  },
+  socialStyle: {
+   flexDirection: 'row',
+   alignItems: 'center',
+   backgroundColor: '#dc4e41',
+   width: Dimensions.get('window').width,
+   backgroundColor: 'rgba(0,0,0,0.6)',
+   padding:10
+ },
+ email: {
+   width: Dimensions.get('window').width,
+   padding:13,
+   marginLeft: 48,
+   fontSize: 18,
+   fontWeight: 'bold',
+   color: "#FFFFFF",
+   backgroundColor: 'rgba(0,0,0,0)',
+   textAlign: 'center',
+ },
   social: {
     width: Dimensions.get('window').width,
     padding:13,
-    fontSize: 20,
-    borderWidth:4,
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: "#FFFFFF",
     backgroundColor: 'rgba(0,0,0,0)',
     textAlign: 'center',
   },
   socialLogo: {
-    justifyContent:'flex-end',
-    padding: 10,
-    margin: 5,
-    height: 50,
-    width: 50,
+    height: 40,
+    width: 40,
     resizeMode : 'stretch',
-  }
+    position: 'absolute',
+    left: 40,
+  },
+  TextStyle :{
+    color: "#fff",
+    marginBottom : 4,
+    marginRight :20,
+  },
 })
 
 export default connect(({routes}) => ({routes}))(Contact)
